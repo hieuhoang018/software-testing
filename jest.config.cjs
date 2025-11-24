473f721f11141bb8b9851a14ba8906ac2c8c12fd
Hoang Vu Hieu <hieu.hoang@tuni.fi>
2025-11-24 22:46:20 +0200

    feat: implemented github actions ci pipeline and coveralls integration

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: "node",
+  collectCoverage: false,
+  coverageReporters: ["text", "lcov"],
 
   // Disable Babel/other transforms so Jest lets Node handle native ESM
   transform: {},
