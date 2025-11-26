53969f59dcd3897a0ae5abe68ac84ee581833429
Minh Nguyen <quangminh240924@gmail.com>
2025-11-26 04:22:12 +0200

    feat: implemented test cases for isEmpty function

---
+++ src/tests/isEmpty.test.js
@@ -0,0 +1,33 @@
+import isEmpty from '../utils/isEmpty'
+
+test('null is empty: isEmpty(null) = true', () => {
+  expect(isEmpty(null)).toBe(true)
+})
+
+test('boolean object is empty: isEmpty(true) = true', () => {
+  expect(isEmpty(true)).toBe(true)
+})
+
+test('a number is empty: isEmpty(1.01) = true', () => {
+  expect(isEmpty(1.01)).toBe(true)
+})
+
+test('a list with no value is empty: isEmpty([]) = true', () => {
+  expect(isEmpty([])).toBe(true)
+})
+
+test('an `arguments` object of a function that have no parameter is empty: isEmpty(function() { return arguments }()) = true', () => {
+  expect(isEmpty(function() { return arguments }())).toBe(true)
+})
+
+test('a list with values is not empty: isEmpty([1, 2, 3]) = false', () => {
+  expect(isEmpty([1, 2, 3])).toBe(false)
+})
+
+test('a string is not empty: isEmpty("abc") = false', () => {
+  expect(isEmpty("abc")).toBe(false)
+})
+
+test('an object with values is not empty: isEmpty({ "a": 1 }) = false', () => {
+  expect(isEmpty({ "a": 1 })).toBe(false)
+})
